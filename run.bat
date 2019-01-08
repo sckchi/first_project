264d26ec7c7dbae31bb495256a60c031699e09d3
sckchi <45672468+sckchi@users.noreply.github.com>
2019-01-07 16:07:06 -0800

    Update run.bat
    Add vcvars32.bat to run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-"c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+"c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat";
 "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\x86_amd64\cl.exe" test3.c /link Ws2_32.lib msvcrt.lib /out:test3.exe
